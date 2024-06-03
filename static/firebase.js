2fe2e53d045009b36d027dba8662ec1d52506009
Harsh chitaliya <124803206+harshchi19@users.noreply.github.com>
2024-06-03 21:06:09 +0530

    Add files via upload

---
+++ static/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCZx97Ql-4sUVM3MLgx2uV5ZjDu3vyJvJY",
+    authDomain: "fir-project-367bb.firebaseapp.com",
+    projectId: "fir-project-367bb",
+    storageBucket: "fir-project-367bb.appspot.com",
+    messagingSenderId: "153616576371",
+    appId: "1:153616576371:web:d77d4c410397b6a23a9f06",
+    measurementId: "G-7CEE433Q8Y"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
